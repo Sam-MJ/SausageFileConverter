39c6c1644881589ec284d042881092ce2f5169bd
Sam-MJ <tf2redspark@gmail.com>
2024-05-16 14:51:58 +0100

    added ip_address and mac_address to transaction

--- telem.py
+++ telem.py
@@ -5,24 +5,31 @@ from dotenv import load_dotenv
 import os
 import datetime
 import json
+import time
 
 
 class SendThread(QtCore.QThread):
 
     has_internet = QtCore.Signal(bool)
+    ip = QtCore.Signal(str)
 
     @QtCore.Slot(str)
-    def set_payload(self, payload):
+    def send_payload(self, payload):
         self.payload = payload
-
-    def run(self):
         database_url = os.getenv("DATABASE_URL")
-        print(f"send first payload {self.payload}")
-        #
+        # database_url = "http://www.soundspruce.com//sausagefileconverter-transactions"
+
         try:
             requests.post(database_url, json=self.payload, timeout=15)
         except Exception as e:
             self.has_internet.emit(False)
+
+        print(f"send first payload {self.payload}")
+
+    def run(self):
+        time.sleep(3)
+        ip_address = requests.get("https://api.ipify.org", timeout=15).text
+        self.ip.emit(ip_address)
 
 
 class Telem(QtCore.QObject):
@@ -34,8 +41,9 @@ class Telem(QtCore.QObject):
         self.ctrl = ctrl
         # data
         self.telem_version = 1
-        self.product_id = 1
         self.session_uuid = uuid.uuid4().hex
+        self.my_mac = hex(uuid.getnode())
+        self.my_ip = None
         self.files_created = 0
         self.files_scanned = 0
         self.session_start = datetime.datetime.now().isoformat()
@@ -48,25 +56,30 @@ class Telem(QtCore.QObject):
 
     def _send_first_request(self):
 
-        self.ip_thread = SendThread()
-        self.ip_thread.has_internet.connect(self._assign_internet)
-        self.payload.connect(self.ip_thread.set_payload)
-        pl = self._get_json_payload()
-        self.payload.emit(pl)
-        self.ip_thread.start()
+        self.first_send_thread = SendThread()
+        self.first_send_thread.has_internet.connect(self._assign_internet)
+        self.first_send_thread.ip.connect(self._assign_ip_to_payload)
+        self.payload.connect(self.first_send_thread.send_payload)
+        self.first_send_thread.start()
 
     def _assign_internet(self, internet_status):
         self.internet_status = internet_status
 
+    def _assign_ip_to_payload(self, ip):
+        self.my_ip = ip
+        pl = self._get_json_payload()
+        self.payload.emit(pl)
+
     def _get_json_payload(self):
         self.files_scanned = self.ctrl["files_scanned"]
         self.files_created = self.ctrl["files_created"]
-        print("get files_scanned len")
+
         self.session_end = datetime.datetime.now().isoformat()
 
         payload = {
             "telem_version": self.telem_version,
-            "product_id": self.product_id,
+            "ip_address": self.my_ip,
+            "mac_address": self.my_mac,
             "session_id": self.session_uuid,
             "files_created": self.files_created,
             "files_scanned": self.files_scanned,
