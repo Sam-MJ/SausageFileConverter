aa398fac90ff423611f602cdfdc924d9698aa617
Sam-MJ <tf2redspark@gmail.com>
2024-05-30 18:11:30 +0100

    removed IP from telem so it can be handled by the server instead

--- src/telem.py
+++ src/telem.py
@@ -11,11 +11,13 @@ import time
 class SendThread(QtCore.QThread):
 
     has_internet = QtCore.Signal(bool)
-    ip = QtCore.Signal(str)
 
-    @QtCore.Slot(str)
-    def send_payload(self, payload):
+    def __init__(self, payload):
+        super().__init__()
         self.payload = payload
+
+    def run(self):
+        # self.payload = payload
 
         self.database_url = os.getenv("DATABASE_URL")
 
@@ -25,24 +27,8 @@ class SendThread(QtCore.QThread):
             self.has_internet.emit(False)
         print(f"send first payload {self.payload}")
 
-        time.sleep(
-            3
-        )  # this thread blocks GUI! :( run method, runs on thread creation. others have to be started first.
-        # because telem thread isn't started on main thread and this thread isn't started by telem, this is still owned by the main thread.  AAAHHH!
-        print("bbbb")
-
-    def run(self):
-
-        ip_address = requests.get("https://api.ipify.org", timeout=15).text
-
-        time.sleep(3)
-        print("aaaa")
-        self.ip.emit(ip_address)
-
 
 class Telem(QtCore.QObject):
-
-    payload = QtCore.Signal(str)
 
     def __init__(self, ctrl) -> None:
         super().__init__(parent=None)
@@ -51,7 +37,6 @@ class Telem(QtCore.QObject):
         self.telem_version = 1
         self.session_uuid = uuid.uuid4().hex
         self.my_mac = hex(uuid.getnode())
-        self.my_ip = None
         self.files_created = 0
         self.files_scanned = 0
         self.session_start = datetime.datetime.now().isoformat()
@@ -64,23 +49,16 @@ class Telem(QtCore.QObject):
 
     def _send_first_request(self):
         """create a thread that can use the run method to fetch ip on creation, the ip is then added to the request payload."""
-        self.first_send_thread = SendThread()
+        payload = self._get_json_payload()
+        self.first_send_thread = SendThread(payload)
 
-        # first send gets IP and checks if there is a successful internet connection
+        # send first payload and see if there is a successful internet connection
         self.first_send_thread.has_internet.connect(self._assign_internet)
-        self.first_send_thread.ip.connect(self._assign_ip_to_payload)
 
-        # once the IP has been set it is added to the payload and sent back to the first send thread.
-        self.payload.connect(self.first_send_thread.send_payload)
         self.first_send_thread.start()
 
     def _assign_internet(self, internet_status):
         self.internet_status = internet_status
-
-    def _assign_ip_to_payload(self, ip):
-        self.my_ip = ip
-        pl = self._get_json_payload()
-        self.payload.emit(pl)
 
     def _get_json_payload(self):
         """populate dictionary with instance variables and then convert to json"""
@@ -92,7 +70,6 @@ class Telem(QtCore.QObject):
 
         payload = {
             "telem_version": self.telem_version,
-            "ip_address": self.my_ip,
             "mac_address": self.my_mac,
             "session_id": self.session_uuid,
             "files_created": self.files_created,
